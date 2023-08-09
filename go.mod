ee6e296d16801760a2fd86d8f265f40dd4d2159e
otvet8 <arsabramoff@gmail.com>
2023-08-09 18:28:05 +0300

    added grpc server

--- go.mod
+++ go.mod
@@ -7,11 +7,17 @@ require (
 	github.com/go-chi/chi v1.5.4
 	github.com/golang-migrate/migrate/v4 v4.16.2
 	github.com/jackc/pgx/v5 v5.4.3
+	google.golang.org/grpc v1.51.0
+	google.golang.org/protobuf v1.28.1
 )
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
 )
--- internal/handlers/grpc/handlers.go
+++ internal/handlers/grpc/handlers.go
@@ -1,1 +1,26 @@
 package grpc
+
+import (
+	"context"
+
+	pb "github.com/AbramovArseniy/Companies/internal/handlers/grpc/proto"
+	db "github.com/AbramovArseniy/Companies/internal/storage/postgres/db"
+)
+
+type CompaniesServer struct {
+	pb.UnimplementedCompaniesServiceServer
+
+	Storage db.Querier
+}
+
+func (s *CompaniesServer) GetTree(context.Context, *pb.GetTreeRequest) (*pb.GetTreeResponse, error) {
+	return nil, nil
+}
+
+func (s *CompaniesServer) GetHierarchy(_ context.Context, req *pb.GetHierarchyRequest) (*pb.GetHierarchyResponse, error) {
+	return nil, nil
+}
+
+func (s *CompaniesServer) GetNode(_ context.Context, req *pb.GetNodeRequest) (*pb.GetNodeResponse, error) {
+	return nil, nil
+}
